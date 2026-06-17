ea3c7adec4bcf769716ea034af82dbda8662299b
vaishh456 <vaishhkuppe@gmail.com>
2026-06-17 11:50:18 +0530

    Add canvas initialization and display functions

---
+++ main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+#define ROWS 20
+#define COLS 40
+
+char canvas[ROWS][COLS];
+
+void initializeCanvas()
+{
+    for(int i=0;i<ROWS;i++)
+    {
+        for(int j=0;j<COLS;j++)
+        {
+            canvas[i][j] = '_';
+        }
+    }
+}
+
+void displayCanvas()
+{
+    for(int i=0;i<ROWS;i++)
+    {
+        for(int j=0;j<COLS;j++)
+        {
+            printf("%c ", canvas[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    initializeCanvas();
+    displayCanvas();
+    return 0;
+}
