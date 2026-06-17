96600817417a80b1ca334cfe4deb27fffa0e701a
vaishh456 <vaishhkuppe@gmail.com>
2026-06-17 13:21:23 +0530

    Fix formatting issues in main.c

--- main.c
+++ main.c
@@ -6,22 +6,24 @@
 
 char canvas[ROWS][COLS];
 
+/* Initialize canvas */
 void initializeCanvas()
 {
-    for(int i=0; i<ROWS; i++)
+    for(int i = 0; i < ROWS; i++)
     {
-        for(int j=0; j<COLS; j++)
+        for(int j = 0; j < COLS; j++)
         {
             canvas[i][j] = '_';
         }
     }
 }
 
+/* Display canvas */
 void displayCanvas()
 {
-    for(int i=0; i<ROWS; i++)
+    for(int i = 0; i < ROWS; i++)
     {
-        for(int j=0; j<COLS; j++)
+        for(int j = 0; j < COLS; j++)
         {
             printf("%c ", canvas[i][j]);
         }
@@ -29,21 +31,29 @@ void displayCanvas()
     }
 }
 
+/* Draw rectangle */
 void drawRectangle(int x, int y, int width, int height)
 {
-    for(int i=x; i<x+width; i++)
+    for(int i = x; i < x + width; i++)
     {
-        canvas[y][i] = '*';
-        canvas[y+height-1][i] = '*';
+        if(y >= 0 && y < ROWS && i >= 0 && i < COLS)
+            canvas[y][i] = '*';
+
+        if(y + height - 1 >= 0 && y + height - 1 < ROWS && i >= 0 && i < COLS)
+            canvas[y + height - 1][i] = '*';
     }
 
-    for(int i=y; i<y+height; i++)
+    for(int i = y; i < y + height; i++)
     {
-        canvas[i][x] = '*';
-        canvas[i][x+width-1] = '*';
+        if(i >= 0 && i < ROWS && x >= 0 && x < COLS)
+            canvas[i][x] = '*';
+
+        if(i >= 0 && i < ROWS && x + width - 1 >= 0 && x + width - 1 < COLS)
+            canvas[i][x + width - 1] = '*';
     }
 }
 
+/* Draw line (DDA algorithm) */
 void drawLine(int x1, int y1, int x2, int y2)
 {
     int dx = x2 - x1;
@@ -57,33 +67,44 @@ void drawLine(int x1, int y1, int x2, int y2)
     float x = x1;
     float y = y1;
 
-    for(int i=0; i<=steps; i++)
+    for(int i = 0; i <= steps; i++)
     {
-        canvas[(int)(y + 0.5)][(int)(x + 0.5)] = '*';
+        int px = (int)(x + 0.5);
+        int py = (int)(y + 0.5);
+
+        if(px >= 0 && px < COLS && py >= 0 && py < ROWS)
+            canvas[py][px] = '*';
+
         x += xInc;
         y += yInc;
     }
 }
 
+/* Draw triangle */
 void drawTriangle(int x, int y, int height)
 {
-    for(int i=0; i<height; i++)
+    for(int i = 0; i < height; i++)
     {
-        canvas[y+i][x-i] = '*';
-        canvas[y+i][x+i] = '*';
+        if(y + i < ROWS && x - i >= 0)
+            canvas[y + i][x - i] = '*';
+
+        if(y + i < ROWS && x + i < COLS)
+            canvas[y + i][x + i] = '*';
     }
 
-    for(int i=x-height+1; i<=x+height-1; i++)
+    for(int i = x - height + 1; i <= x + height - 1; i++)
     {
-        canvas[y+height-1][i] = '*';
+        if(y + height - 1 < ROWS && i >= 0 && i < COLS)
+            canvas[y + height - 1][i] = '*';
     }
 }
 
+/* Draw circle (simple filled circle) */
 void drawCircle(int centerX, int centerY, int radius)
 {
-    for(int y=0; y<ROWS; y++)
+    for(int y = 0; y < ROWS; y++)
     {
-        for(int x=0; x<COLS; x++)
+        for(int x = 0; x < COLS; x++)
         {
             int dx = x - centerX;
             int dy = y - centerY;
@@ -95,10 +116,13 @@ void drawCircle(int centerX, int centerY, int radius)
         }
     }
 }
+
+/* Clear canvas */
 void clearCanvas()
 {
     initializeCanvas();
 }
+
 int main()
 {
     int choice;
@@ -116,6 +140,7 @@ int main()
         printf("6. Clear Canvas\n");
         printf("7. Exit\n");
         printf("Enter choice: ");
+
         scanf("%d", &choice);
 
         switch(choice)
@@ -150,8 +175,8 @@ int main()
                 break;
 
             case 7:
-               printf("Exiting...\n");
-               break;
+                printf("Exiting...\n");
+                break;
 
             default:
                 printf("Invalid Choice!\n");
